37d7f6e92b2e311343ec68d3c7a6d70ba9445afa
Kousuke Ebihara <ebihara@tejimaya.com>
2009-09-21 16:19:42 +0900

    Initialized a new project

---
+++ config/ProjectConfiguration.class.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once '/usr/share/php/symfony-1.3-dev/lib/autoload/sfCoreAutoload.class.php';
+sfCoreAutoload::register();
+
+class ProjectConfiguration extends sfProjectConfiguration
+{
+  public function setup()
+  {
+    $this->enablePlugins('sfDoctrinePlugin');
+  }
+}
---
+++ lib/form/BaseForm.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Base project form.
+ * 
+ * @package    examine-template
+ * @subpackage form
+ * @author     ##AUTHOR_NAME## 
+ * @version    SVN: $Id: BaseForm.class.php 20147 2009-07-13 11:46:57Z FabianLange $
+ */
+class BaseForm extends sfFormSymfony
+{
+}
